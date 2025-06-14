4ed6c6272c9ab8a3d79481b915d7e3ed77918c5f
santwan <santwanpathak1@gmail.com>
2025-06-14 20:56:14 +0530

    Create animeList.js

---
+++ src/data/animeList.js
@@ -0,0 +1,26 @@
+const animeList = [
+    {
+      id: 1,
+      title: "Attack on Titan",
+      genre: ["Action", "Drama"],
+      rating: 9.5,
+      image: "https://upload.wikimedia.org/wikipedia/en/7/70/Shingeki_no_Kyojin_manga_volume_1.jpg"
+    },
+    {
+      id: 2,
+      title: "Your Name",
+      genre: ["Romance", "Drama"],
+      rating: 9.0,
+      image: "https://upload.wikimedia.org/wikipedia/en/0/0b/Your_Name_poster.png"
+    },
+    {
+      id: 3,
+      title: "Demon Slayer",
+      genre: ["Action", "Adventure"],
+      rating: 9.2,
+      image: "https://upload.wikimedia.org/wikipedia/en/6/6f/Kimetsu_no_Yaiba_Volume_1.png"
+    }
+  ];
+  
+  export default animeList;
+    
